695417d9ad2d356cabd611226c92bb2f88554ed8
bertomeu <josecarlosbertomeu@gmail.com>
2024-07-29 18:37:50 +0000

    modified layout.js

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,12 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { CreateContact } from "./component/createContact";
+import NavbarTop from "./component/navbarTop";
 
 //create your first component
 const Layout = () => {
@@ -18,19 +18,20 @@ const Layout = () => {
 
 	return (
 		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
+		<BrowserRouter basename={basename}>
+			<ScrollToTop>
+				<NavbarTop />
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/edit-contact/:id" element={<CreateContact />} />
+					<Route path="/create-contact" element={<CreateContact />} />
+					<Route path="*" element={<h1>Not found!</h1>} />
+				</Routes>
+				<Footer />
+			</ScrollToTop>
+		</BrowserRouter>
+	</div>
 	);
 };
 
