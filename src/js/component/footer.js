1e7aeb33de5e2e9b3c7ff9c40470311a4fb16cf5
bertomeu <josecarlosbertomeu@gmail.com>
2024-07-29 18:26:59 +0000

    modified footer.js

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+    <footer className="footer mt-auto py-3 text-center text-body-emphasis">
+        <p>
+        Made with <i className="fa-brands fa-react text-info"></i> by Jose Carlos Bertomeu
+        </p>
+    </footer>
 );
+
