e912b0931d56a60080607e17ad303bfb398f95d0
bertomeu <josecarlosbertomeu@gmail.com>
2024-07-29 18:39:20 +0000

    modified flux.js

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,93 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+    return {
+        store: {
+            contacts: [],
+            showModal: false,
+            selectedContactId: null,
+            
+        },
+        actions: {
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+            setShowModal: (value) => {
+                setStore({ showModal: value });
+            },
+            
+            setSelectedContactId: (id) => {
+                setStore({ selectedContactId: id });
+            },
+         
+
+            getContacts: () => {
+                fetch("https://playground.4geeks.com/contact/agendas/bertomeu/contacts", {
+                    method: "GET"
+                })
+                    .then(response => response.json())
+                    .then((data) => {
+                        setStore({ contacts: data.contacts });
+                    })
+                    .catch((err) => { console.error(err) })
+            },
+            createAgenda: () => {
+                fetch("https://playground.4geeks.com/contact/agendas/bertomeu/", {
+                    method: "GET"
+                })
+                    .then(response =>
+                        response.status === 404
+                            ? fetch("https://playground.4geeks.com/contact/agendas/bertomeu/", { method: "POST" })
+                            : response.ok
+                                ? response.json()
+                                : null
+                    )
+                    .then((data) => {
+                        setStore(data.store);
+                    })
+                    .catch((err) => { console.error(err) })
+            },  
+            createContact: (contact) => {
+                fetch("https://playground.4geeks.com/contact/agendas/bertomeu/contacts", {
+                    method: "POST",
+                    headers: { "Content-type": "application/json" },
+                    body: JSON.stringify(contact)
+                })
+                .then(response => response.json())
+                .then(() => {
+                    getActions().getContacts();
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+            },  
+            editContact: (id, contactData) => {
+                fetch(`https://playground.4geeks.com/contact/agendas/bertomeu/contacts/${id}`, {
+                    method: "PUT",
+                    headers: { "Content-type": "application/json" },
+                    body: JSON.stringify(contactData),
+                })
+                .then(response => response.json())
+                .then(() => {
+                    getActions().getContacts();
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+            },
+            deleteContact: (id) => {
+                fetch(`https://playground.4geeks.com/contact/agendas/bertomeu/contacts/${id}`, {
+                    method: "DELETE",
+                })
+                    .then(response => {
+						console.log(response);
+						if (response.ok === true) {
+							getActions().getContacts();
+						}
+												
+						})
+                   
+                    .catch((error) => console.error("deleting contact failed:", error));
+            },
+        }
+    };
 };
 
 export default getState;
